345d8ccfe774cd256b91af89ea8b752f4f98f261
Nguyễn Mạnh Dũng <nguyenmanhdungtk@gmail.com>
2019-03-20 18:57:28 +0700

    Revert "update Playing vs SongList"
    This reverts commit bb0ff463

--- src/screens/Playing.js
+++ src/screens/Playing.js
@@ -6,79 +6,102 @@ import PlayingWidget from "../components/PlayingWidget";
 
 import TrackPlayer from 'react-native-track-player';
 import {colors} from "../utils";
-import store from "../store";
 
 const ARTWORK_SIZE = Dimensions.get('screen').height * 0.35;
 
 export default class Playing extends Component {
-	currentPlaySong = store.getState().songs[store.getState().currentPlayIndex];
 
-	async componentDidMount() {
-		await TrackPlayer.setupPlayer();
-		TrackPlayer.updateOptions({
-			stopWithApp: true,
-			capabilities: [
-				TrackPlayer.CAPABILITY_PLAY,
-				TrackPlayer.CAPABILITY_PAUSE,
-				TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
-			],
-			compactCapabilities: [
-				TrackPlayer.CAPABILITY_PLAY,
-				TrackPlayer.CAPABILITY_PAUSE
-			],
-			color: '#FF9ACD32'
-		});
+    constructor(props){
+        super(props);
+        this.state = {
+            title : "",
+            artist : "",
+        }
+    }
+    // static navigationOptions = {
+    //     title: this.state.title,
+    // };
 
-		const song = this.currentPlaySong;
-		let track = {
-			...song,
-			url: song.uri,
-		};
+    async componentDidMount() {
+        await TrackPlayer.setupPlayer();
+        TrackPlayer.updateOptions({
+            stopWithApp: true,
+            capabilities: [
+                TrackPlayer.CAPABILITY_PLAY,
+                TrackPlayer.CAPABILITY_PAUSE,
+                TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+            ],
+            compactCapabilities: [
+                TrackPlayer.CAPABILITY_PLAY,
+                TrackPlayer.CAPABILITY_PAUSE
+            ],
+            color: '#FF9ACD32'
+        });
+        this.setState({
+            title: this.props.navigation.getParam("title"),
+            artist: this.props.navigation.getParam("artist"),
+        });
+        let track = {
+            id:  this.props.navigation.getParam("id"),
+            url: this.props.navigation.getParam("uri"),
+            title:this.props.navigation.getParam("title"),
+            artist: this.props.navigation.getParam("artist"),
+            album: this.props.navigation.getParam("albumArtist"),
+            genre: '',
+            date: '2014-05-20T07:00:00+00:00',
+            artwork: 'https://picsum.photos/600/600',
+            duration: this.props.navigation.getParam("duration"),
+            description: this.props.navigation.getParam("title") ,
+        };
 
-		await TrackPlayer.add(track);
-		await TrackPlayer.play();
-	}
+        console.log("=================== STATE =============")
+        console.log(this.state);
+        console.log("============ TRACK =============");
+        console.log(track);
+        
 
-	render() {
-		const {title, artist, artwork} = this.currentPlaySong;
+        await TrackPlayer.add(track);
+        await TrackPlayer.play();
+    }
 
-		return (
-			<View style={styles.container}>
-				<Text style={styles.nowPlaying}>NOW PLAYING</Text>
-				<Avatar
-					uri={artwork}
-					width={ARTWORK_SIZE}
-					elevation={30}
-				/>
-				<SongArtist
-					songTitle={title}
-					artist={artist}
-					songSize={25}
-					artistSize={18}
-					wrapperStyle={styles.songAuthor}
-				/>
-				<PlayingWidget/>
-			</View>
-		);
-	}
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.nowPlaying}>NOW PLAYING</Text>
+                <Avatar
+                    uri={""}
+                    width={ARTWORK_SIZE}
+                    elevation={30}
+                />
+                <SongArtist
+	                song={this.state.title}
+	                artist={this.state.artist}
+	                songSize={25}
+	                artistSize={18}
+	                wrapperStyle={styles.songAuthor}
+                />
+                <PlayingWidget/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: colors.white,
-	},
-	nowPlaying: {
-		flexGrow: 1,
-		textAlignVertical: 'bottom',
-		color: colors.grey,
-		marginBottom: 20,
-		letterSpacing: 3,
-	},
-	songAuthor: {
-		alignItems: 'center',
-		marginTop: 20
-	},
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.white,
+    },
+    nowPlaying: {
+        flexGrow: 1,
+        textAlignVertical: 'bottom',
+        color: colors.grey,
+        marginBottom: 20,
+        letterSpacing: 3,
+    },
+    songAuthor: {
+        alignItems: 'center',
+        marginTop: 20
+    },
 });
