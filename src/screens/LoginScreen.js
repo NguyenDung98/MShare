cfc7734b7176a087387acb10a2169de7ee0277c1
huyenhuyen1204 <nguyenhuyen98pm@egmail.com>
2019-03-28 11:07:58 +0700

    LoginScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,51 +1,40 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import { View, Button } from 'react-native';
+// import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import { LoginManager, AccessToken } from "react-native-fbsdk";
  
 export default class Login extends Component{
+  constructor(props){
+    super(props);
+  }
+  _fbAuth(self){
+    LoginManager.logInWithReadPermissions(["public_profile"]).then(
+      function(result) {
+        if (result.isCancelled) {
+          console.log("Login cancelled");
+        } else {
+          console.log(
+            "Login success with permissions: " +
+              result.grantedPermissions.toString()
+          );
+          AccessToken.getCurrentAccessToken().then(
+            (data)=>{
+              console.log(data.accessToken.toString());
+            }
+          );
+          self.props.navigation.navigate("TabScreen");
+        }
+      },
+      function(error) {
+        console.log("Login fail with error: " + error);
+      }
+    );
+  }
   render() {
     return (
       <View>
-        <LoginButton
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                console.log("login has error: " + result.error);
-                console.log("RESULT LOG:");
-                console.log(result);
-                console.log("ERROR LOG: " + error)
-              } else if (result.isCancelled) {
-                console.log("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    console.log(data.accessToken.toString())
-                  }
-                )
-              }
-            }
-          }
-          onLogoutFinished={() => console.log("logout.")}/>
-        {/* <LoginButton
-          publishPermissions={["publish_actions"]}
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                alert("login has error: " + result.error);
-              } else if (result.isCancelled) {
-                alert("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    alert(data.accessToken.toString())
-                  }
-                )
-              }
-            }
-          }
-          onLogoutFinished={() => alert("logout.")}/> */}
+          <Button onPress={()=> {this._fbAuth(this);}} title='FB Login' />
       </View>
     );
   }
 };
-
