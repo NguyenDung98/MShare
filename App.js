bf8e3333dee3615232a0b253cd6881bd9ff69f71
Nguyễn Mạnh Dũng <nguyenmanhdungtk@gmail.com>
2019-04-11 00:18:20 +0700

    update

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import firebase from '@firebase/app';
+import '@firebase/storage';
+
 // <<<<<<< HEAD
 // import Playing from "./src/screens/Playing";
 // import SongList from "./src/screens/SongList";
@@ -7,7 +10,22 @@ import AppNavigator from "./src/navigation/AppNavigator"
 // >>>>>>> huyen
 
 export default class App extends Component {
-    render() {
+	componentDidMount() {
+		const config = {
+			apiKey: "AIzaSyC7XlAPLaPQng-o0zBqqco-sumoI8oXnm0",
+			authDomain: "mshare-d1155.firebaseapp.com",
+			databaseURL: "https://mshare-d1155.firebaseio.com",
+			projectId: "mshare-d1155",
+			storageBucket: "mshare-d1155.appspot.com",
+			messagingSenderId: "53496378186"
+		};
+		firebase.initializeApp(config);
+		let storage = firebase.storage();
+		let songRef = storage.ref('music/BeautifulInWhite-ShaneFilan-524801.mp3');
+		console.log(songRef);
+	}
+
+	render() {
         return <AppNavigator/>
     }
 }
