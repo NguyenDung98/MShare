8778b412d3d54b2b42a5044971d35e4004e96418
huyenhuyen1204 <nguyenhuyen98pm@egmail.com>
2019-04-22 20:46:08 +0700

    login FB

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { View, Button , Text} from 'react-native';
+import { View, Button , Text, Alert} from 'react-native';
 // import { LoginButton, AccessToken } from 'react-native-fbsdk';
 import { LoginManager, AccessToken } from "react-native-fbsdk";
+import { saveAccessToken, getAccessToken } from '../utils/asyncStorage';
  
 export default class Login extends Component{
   constructor(props){
     super(props);
   }
   _fbAuth(self){
-    LoginManager.logInWithReadPermissions(["public_profile"]).then(
+    LoginManager.logInWithReadPermissions(["public_profile", "user_friends"]).then(
       function(result) {
         if (result.isCancelled) {
           console.log("Login cancelled");
@@ -19,16 +20,64 @@ export default class Login extends Component{
           );
           AccessToken.getCurrentAccessToken().then(
             (data)=>{
-              console.log(data.accessToken.toString());
+              saveAccessToken(data.accessToken.toString());
+              console.log(getAccessToken())
+              
+              // console.log(data.accessToken.toString());
+              self.props.navigation.navigate("TabScreen");
+ 
             }
           );
-          self.props.navigation.navigate("TabScreen");
         }
       },
       function(error) {
         console.log("Login fail with error: " + error);
       }
     );
+    // LoginManager.logInWithReadPermissions(['public_profile', 'user_friends'])
+    //                 .then((result) => {
+    //                     console.log('RESULT ' + result);
+    //                     if(result.isCancelled){
+    //                         return Promise.reject(new Error('the user cancle request'))
+    //                     }
+    //                     console.log('Login success: ' + result.grantedPermissions.toString())
+    //                     return AccessToken.getCurrentAccessToken();
+    //                 })
+    //                 .then((data) =>{    
+    //                     const firebase = require('firebase');
+    //                     const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
+    //                     return firebase.auth().signInAndRetrieveDataWithCredential(credential);
+    //                 })
+    //                 .then((currentUser) =>{
+    //                     console.log(JSON.stringify(currentUser));
+    //                     // this.props.navigation.navigate('TabScreen');
+
+    //                 }).catch((error) => {
+    //                     console.log('ERROR '+  error);
+    //                     Alert.alert(
+    //                         'Sorry',
+    //                         '' + error,
+    //                         [
+    //                         {text: 'OK', onPress: () => console.log('OK Pressed')},
+    //                         ],
+    //                         { cancelable: false }
+    //                     )
+    //                 })
+    // LoginManager.logInWithReadPermissions(["public_profile", "user_friends"]).then(
+    //   function(result) {
+    //     if (result.isCancelled) {
+    //       console.log("Login cancelled");
+    //     } else {
+    //       console.log(
+    //         "Login success with permissions: " +
+    //           result.grantedPermissions.toString()
+    //       );
+    //     }
+    //   },
+    //   function(error) {
+    //     console.log("Login fail with error: " + error);
+    //   }
+    // );
   }
   render() {
     return (
---
+++ src/utils/asyncStorage.js
@@ -0,0 +1,18 @@
+import { AsyncStorage } from 'react-native'
+
+export const getAccessToken = async () => {
+    try {
+        const token = await AsyncStorage.getItem('@token08022019:key');
+        return (token !== null) ? token : '';
+    } catch (error) {
+        return '';
+    }
+};
+
+export const saveAccessToken = async (token = '') => {
+    try {
+        await AsyncStorage.setItem('@token08022019:key', token);
+    } catch (e) {
+        console.log(e);
+    }
+};
